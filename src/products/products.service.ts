948e44340a11b8f5d7151c9b46e506abd52ce364
kblo4517 <50221450+kblo4517@users.noreply.github.com>
2023-05-25 21:52:42 -0600

    2 files modified

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,14 +1,59 @@
-import { Injectable} from '@nestjs/common';
+import { Injectable, NotFoundException} from '@nestjs/common';
 import {Product} from './product.model'
 
 @Injectable()
 export class ProductsService{
-    products: Product[] = [];
+    private products: Product[] = [];
 
     insertProduct(title: string, desc: string, price: number) {
       const newProduct = new Product(new Date().toString(), title, desc, price);
-      const prodID = new Date().toString();
+      const prodID = Math.random().toString();
       this.products.push(newProduct);
       return prodID;
+    }
+
+    getProducts() {
+      return [...this.products];
+    }
+
+    getSingleProduct(productId: string) {
+      const product = this.findProduct(productId)[0];
+    
+      return {...product};
+    }
+
+    updateProduct(productId: string, title: string, desc: string, price: number) {
+      const [product, index] = this.findProduct(productId)
+
+      const updatedProduct = {...product}
+      if (title) {
+        updatedProduct.title = title;
+      }
+      if (desc) {
+        updatedProduct.description = desc;
+      }
+      if (price) {
+        updatedProduct.price = price;
+      }
+
+      this.products[index]  = updatedProduct;
+
+    }
+
+    private findProduct(id: string): [Product, number] {
+      const productIndex = this.products.findIndex(prod => prod.id === id)
+
+      const product = this.products[productIndex]
+      if (!product) {
+        throw new NotFoundException('Could not find product');
+      }
+
+      return [product, productIndex];
+    }
+
+    deleteProduct(prodId: string) {
+      const index = this.findProduct(prodId)[1];
+
+      this.products.splice(index, 1);
     }
 }
